4848a9c3c189cbaf21f65b133a72923c9b5e7272
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-04-22 17:39:12 -0600

    Add padding on every viewport

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,7 +1,10 @@
 const Navbar = () => {
   return (
-    <div className="">
-      Navbar
+    <div className="h-full flex items-center justify-between
+    px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+      <div>1</div>
+      <div>2</div>
+
     </div>
   )
 }
