8e44bbfaacee799681d53a2950338ebdb9634074
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-04-22 18:26:26 -0600

    Add a condition to render the menu list and add onclick to show the list

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,4 +1,7 @@
+"use client"
+
 import Link from "next/link";
+import { useState } from "react";
 
 const links = [
   { url: "/", title: "Home" },
@@ -8,6 +11,7 @@ const links = [
 ];
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
 
   return (
     <div className="h-full flex items-center justify-between
@@ -24,18 +28,23 @@ const Navbar = () => {
       {/* Responsive Menu*/}
       <div className="">
         {/* MENU BUTTON */}
-        <button className="w-10 h-8 flex flex-col justify-between">
-          <div className="w-10 h-1 bg-black rounded"></div>
-          <div className="w-10 h-1 bg-black rounded"></div>
-          <div className="w-10 h-1 bg-black rounded"></div>
+        <button
+          className="w-10 h-8 flex flex-col justify-between z-50 relative"
+          onClick={() => setOpen(!open)}
+        >
+          <div className="w-10 h-1 bg-white rounded"></div>
+          <div className="w-10 h-1 bg-white rounded"></div>
+          <div className="w-10 h-1 bg-white rounded"></div>
         </button>
         {/* MENU LIST */}
-        <div className="absolute top-0 left-0 w-screen h-screen bg-black
+        {open && (
+          <div className="absolute top-0 left-0 w-screen h-screen bg-black
          text-white flex flex-col items-center justify-center gap-8 text-4xl">
-          {links.map((link) => (
-            <Link key={link.title} href={link.url}>{link.title}</Link>
-          ))}
-        </div>
+            {links.map((link) => (
+              <Link key={link.title} href={link.url}>{link.title}</Link>
+            ))}
+          </div>)
+        }
       </div>
     </div>
   )
