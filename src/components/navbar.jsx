141fe97f3f11dd0eac19bb60163d8ad7db5c6be0
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-04-22 18:03:50 -0600

    Add menu button using tailwind css

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,9 +1,27 @@
+import Link from "next/link";
+
 const Navbar = () => {
   return (
     <div className="h-full flex items-center justify-between
     px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
-      <div>1</div>
-      <div>2</div>
+      {/* Logo */}
+      <div className="">
+        <Link href="/" className="text-sm bg-black rounded-md p-1 font-semibold
+        flex items-center justify-center">
+          <span className="text-white mr-1">Reyna</span>
+          <span className="w-12 h-8 rounded bg-white text-black
+          flex items-center justify-center">.dev</span>
+        </Link>
+      </div>
+      {/* Responsive Menu*/}
+      <div className="">
+        {/* MENU BUTTON */}
+        <button className="w-10 h-8 flex flex-col justify-between">
+          <div className="w-10 h-1 bg-black rounded"></div>
+          <div className="w-10 h-1 bg-black rounded"></div>
+          <div className="w-10 h-1 bg-black rounded"></div>
+        </button>
+      </div>
 
     </div>
   )
