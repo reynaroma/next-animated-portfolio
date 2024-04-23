565d8cb6649099c8d98e7f493039348b96e90de3
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-04-22 20:18:00 -0600

    Add conditional styling on the navlinks

--- src/components/navLink.jsx
+++ src/components/navLink.jsx
@@ -3,13 +3,14 @@ import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 
 const NavLink = ({ link }) => {
-
+  // Get the current path
   const pathName = usePathname();
   console.log(pathName);
 
   return (
     <Link
-      className="" href={link.url}>{link.title}</Link>
+      className={`rounded p-1 ${pathName === link.url &&
+        "bg-black text-white"}`} href={link.url}>{link.title}</Link>
   );
 };
 
