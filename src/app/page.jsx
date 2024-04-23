7aad7216585c008b920961566d36d82324293849
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-04-23 15:23:45 -0600

    Modify the Homepage component and make display the hero image

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,19 @@
+import Image from "next/image";
+
 const Homepage = () => {
-  return <div>Hello World</div>;
+  return (
+    <div className="h-full flex flex-col">
+      {/* IMAGE CONTAINER */}
+      <div className="h-1/2 relative">
+        <Image src="/hero.png" alt="" fill />
+      </div>
+      {/* TEXT CONTAINER */}
+      <div className="h-1/2">
+
+      </div>
+
+    </div>
+  )
 };
 
 export default Homepage;
