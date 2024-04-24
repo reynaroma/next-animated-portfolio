63b75ef0fbeb88d6e8a8038049f18b68221ba6f7
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-04-24 16:33:48 -0600

    Add anoter experience list item

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -169,8 +169,36 @@ const AboutPage = () => {
                 </div>
 
                 {/* RIGHT  */}
-                <div className="w-1/3">
+                <div className="w-1/3"></div>
 
+              </div>
+              {/* EXPERIENCE LIST ITEM */}
+              <div className="flex justify-between h-48">
+
+                {/* LEFT */}
+                <div className="w-1/3"></div>
+
+                {/* CENTER */}
+                <div className="w-1/6">
+                  {/* LINE */}
+                  <div className="w-1 h-full bg-gray-600 rounded relative">
+                    {/* LINE CIRCLE */}
+                    <div className="absolute w-5 h-5 rounded-full ring-4 
+                    ring-red-400 bg-white -left-2"></div>
+                  </div>
+                </div>
+
+                {/* RIGHT  */}
+                <div className="w-1/3">
+                  {/* JOB TITLE */}
+                  <div className="bg-white p-3 font-semibold 
+                  rounded-b-lg rounded-s-lg">Web Developer</div>
+                  {/* JOB DESCRIPTION */}
+                  <div className="p-2 text-sm italic">My current employment</div>
+                  {/* JOB DATE  */}
+                  <div className="p-3 text-red-400 text-sm font-semibold">Nov 2023 - Feb 2024</div>
+                  {/* JOB COMPANY */}
+                  <div className="p-1 rounded bg-white text-sm font-semibold w-fit">Free Periods Canada</div>
                 </div>
 
               </div>
