7bdc5e2236bd4a2d392f50c34aa7f16e788463e1
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-04-25 16:41:33 -0600

    Add new div for My Works section

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -19,7 +19,10 @@ const PortfolioPage = () => {
     >
       <div
         className="h-[600vh]"
-        ref={ref}></div>
+        ref={ref}>
+        <div className="w-screen h-[calc(100vh-6rem)]
+          flex items-center justify-center text-8xl text-center">My Works</div>
+      </div>
     </motion.div>
   );
 };
