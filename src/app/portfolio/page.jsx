e448a4928e66756f0abdda08f538e29ab6710c32
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-04-25 16:32:38 -0600

    Use ref hook to animate the element

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -1,7 +1,15 @@
 "use client"
-import { motion } from "framer-motion";
+import { motion, useScroll, useTransform } from "framer-motion";
+import { useRef } from "react";
 
 const PortfolioPage = () => {
+  // Create a reference to the element we want to animate
+  const ref = useRef();
+  // Get the scroll progress
+  const { scrollYProgress } = useScroll({ target: ref });
+  // Map the scroll progress to the element's y position
+  const x = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
+
   return (
     <motion.div
       className="h-full"
@@ -9,7 +17,9 @@ const PortfolioPage = () => {
       animate={{ y: "0%" }}
       transition={{ duration: 1 }}
     >
-      Portfolio
+      <div
+        className="h-[600vh]"
+        ref={ref}></div>
     </motion.div>
   );
 };
