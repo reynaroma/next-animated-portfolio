70fdd56dbfdc63f44a8022f001d66f2caa5797e8
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-04-25 16:56:33 -0600

    Add a constants folder then store the object for the portfolio list items

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -20,8 +20,11 @@ const PortfolioPage = () => {
       <div
         className="h-[600vh]"
         ref={ref}>
-        <div className="w-screen h-[calc(100vh-6rem)]
+        <div className="relative w-screen h-[calc(100vh-6rem)]
           flex items-center justify-center text-8xl text-center">My Works</div>
+      </div>
+      <div className="sticky top-0 flex h-screen gap-4 items-center">
+        
       </div>
     </motion.div>
   );
