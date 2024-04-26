0da5b0aa1d1ac911ae8b177a0f0ad1e29cfa8a4a
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-04-26 17:21:59 -0600

    Style the contact section and make some structure to create the elements

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -2,6 +2,9 @@
 import { motion } from "framer-motion";
 
 const ContactPage = () => {
+
+  const text = "Say Hello";
+
   return (
     <motion.div
       className="h-full"
@@ -9,7 +12,29 @@ const ContactPage = () => {
       animate={{ y: "0%" }}
       transition={{ duration: 1 }}
     >
-      Contact
+      <div
+        className="h-fulll flex flex-col lg:flex-row
+        px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+        {/* TEXT CONTAINER */}
+        <div className="h-1/2 lg:h-full lg:w-1/2">
+          <motion.div>{text.split("").map((letter, index) => (
+            <motion.span
+              key={index}
+              initial={{ opacity: 1 }}
+              animate={{ opacity: 0 }}
+              transition={
+                {
+                  duration: 3,
+                  repeat: Infinity,
+                  delay: index * 0.1,
+                }
+              }
+            >{letter}</motion.span>
+          ))}</motion.div>
+        </div>
+        {/* FORM CONTAINER */}
+        <div className="h-1/2 lg:h-full lg:w-1/2"></div>
+      </div>
     </motion.div>
   );
 };
