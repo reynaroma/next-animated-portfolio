3c5b7ecd0471a5ecf25125902adbd5eeb85dbae1
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-04-26 14:05:02 -0600

    Display the text of the SVG and fix the issue

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -56,22 +56,34 @@ const PortfolioPage = () => {
       </div>
       <div className="w-screen h-screen flex flex-col gap-16
           items-center justify-center text-center">
-        <h1 className="">
+        <h1 className="text-8xl">
           Do you have a project?
         </h1>
         <div className="relative">
-          <svg>
+          <motion.svg
+            animate={{ rotate: 360 }}
+            transition={{ duration: 8, ease: "linear", repeat: Infinity }}
+            viewBox="0 0 300 300"
+            className="w-64 h-64 md:w-[500px] md:h-[500px]"
+          >
             <defs>
               <path
                 id="circlePath"
                 d="M 150, 150 m -60, 0 a 60,60 0 0,1 120,0 a 60,60 0 0,1 -120,0 "
               />
             </defs>
-          </svg>
+            <text fill="#000">
+              <textPath
+                xlinkHref="#circlePath"
+                className="text-xl">
+                Web Developer Software Developer</textPath>
+            </text>
+          </motion.svg>
           <Link
             href="/contact"
             className="w-16 h-16 md:w-28 md:h-28 absolute top-0 left-0
-              right-0 bottom-0 m-auto bg-black text-white rounded-full"
+              right-0 bottom-0 m-auto bg-black text-white rounded-full
+              flex items-center justify-center"
           >Hire Me</Link>
         </div>
       </div>
