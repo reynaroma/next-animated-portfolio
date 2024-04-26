9c575c38ec13947f6f608d3a3260916d4ced3b58
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-04-26 17:30:11 -0600

    Fix the typo error

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -13,10 +13,11 @@ const ContactPage = () => {
       transition={{ duration: 1 }}
     >
       <div
-        className="h-fulll flex flex-col lg:flex-row
+        className="h-full flex flex-col lg:flex-row
         px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
         {/* TEXT CONTAINER */}
-        <div className="h-1/2 lg:h-full lg:w-1/2">
+        <div className="h-1/2 lg:h-full lg:w-1/2 flex items-center
+        justify-center text-6xl">
           <div>{text.split("").map((letter, index) => (
             <motion.span
               key={index}
@@ -33,7 +34,7 @@ const ContactPage = () => {
           ))}😊</div>
         </div>
         {/* FORM CONTAINER */}
-        <div className="h-1/2 lg:h-full lg:w-1/2"></div>
+        <form className="h-1/2 lg:h-full lg:w-1/2"></form>
       </div>
     </motion.div>
   );
