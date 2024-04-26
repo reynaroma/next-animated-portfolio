21045a0b727fc76d48e5f40462a985884b12da6a
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-04-25 18:06:38 -0600

    Fix the styling issue

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -25,25 +25,29 @@ const PortfolioPage = () => {
         ref={ref}>
         <div className="w-screen h-[calc(100vh-6rem)]
           flex items-center justify-center text-8xl text-center">My Works</div>
-      </div>
-      <div className="sticky top-0 flex h-screen gap-4 items-center">
-        {items.map((item) => (
-          <div
-            className={`h-screen w-screen flex items-center
+
+        <div className="sticky top-0 flex h-screen gap-4 items-center
+      overflow-hidden">
+          <div className="flex">
+            {items.map((item) => (
+              <div
+                className={`h-screen w-screen flex items-center
             justify-center bg-gradient-to-r ${item.color}`}
-            key={item.id}>
-            <div className="flex flex-col gap-8 text-white">
-              <h1 className="">{item.title}</h1>
-              <div className="relative">
-                <Image src={item.img} alt={item.title} fill />
+                key={item.id}>
+                <div className="flex flex-col gap-8 text-white">
+                  <h1 className="">{item.title}</h1>
+                  <div className="relative">
+                    <Image src={item.img} alt={item.title} fill />
+                  </div>
+                  <p className="">{item.desc}</p>
+                  <Link href={item.link}>
+                    <button>See Demo</button>
+                  </Link>
+                </div>
               </div>
-              <p className="">{item.desc}</p>
-              <Link href={item.link}>
-                <button>See Demo</button>
-              </Link>
-            </div>
+            ))}
           </div>
-        ))}
+        </div>
       </div>
     </motion.div>
   );
