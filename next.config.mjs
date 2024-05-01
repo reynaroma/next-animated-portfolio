4e5d1f3d75364e41e0c7ae0a58aca5a48a34598d
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-04-30 21:20:15 -0600

    Move the loaderFile to the outer part of braces

--- next.config.mjs
+++ next.config.mjs
@@ -2,10 +2,11 @@
 const nextConfig = {
   output: 'export',
   images: {
+    loader: 'custom',
+    loaderFile: './loader.js',
     remotePatterns: [
       {
-        loader: 'custom',
-        loaderFile: './loader.js',
+
         protocol: "https",
         hostname: "images.pexels.com",
         port: "",
