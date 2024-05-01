e787fa5c9735672ef4efea44a7a6032bbb9afbaf
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-04-30 20:38:36 -0600

    Change the background color of the portfolio section

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -33,11 +33,11 @@ const PortfolioPage = () => {
             style={{ x }}
             className="flex">
             <div className='h-screen w-screen flex items-center
-            justify-center bg-black'/>
+            justify-center bg-gradient-to-r from-purple-300 to-red-300"'/>
             {items.map((item) => (
               <div
                 className={`h-screen w-screen flex items-center
-            justify-center bg-black`}
+            justify-center bg-gradient-to-r ${item.color}`}
                 key={item.id}>
                 <div className="flex flex-col gap-8 text-white">
                   <h1 className="text-xl font-bold md:text-4xl
