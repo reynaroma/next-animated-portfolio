de4b8c84505c87b506001c347c9100de77ae04b3
Reyna May Roma <130502736+reynaroma@users.noreply.github.com>
2024-04-30 21:42:04 -0600

    Modfiy the loader file

--- loader.js
+++ loader.js
@@ -1,3 +1,3 @@
 export default function myImageLoader({ src, width, quality }) {
-  return `https://reynamayromaportfolio.com/${src}?w=${width}&q=${quality || 75}`
+  return `https://next-animated-portfolio-reynaroma.vercel.app/${src}?w=${width}&q=${quality || 75}`
 }
